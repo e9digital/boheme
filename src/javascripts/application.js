6f15629f63412b1d4ddd5979e3358749c04c88a0
Travis <numbers1311407@gmail.com>
2011-04-01 18:59:09 -0400

    removed extra js

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,63 +1,53 @@
 ;jQuery(function($) {
 
-    var checkboxes = $('input[type=checkbox]');
+  var checkboxes = $('input[type=checkbox]');
 
-    checkboxes.each(function(){
-
-      var label = $('label[for='+ $(this).attr('id') +']');
-      label.mouseover(function(){
-        $(this).addClass('hover');
-        if( $(this).is('.checked') ){
-          $(this).addClass('checkedHover');
-        }
-      });
-      label.mouseout(function(){
-        $(this).removeClass('hover checkedHover');
-      });
-
-      $(this).add('label').wrap('<div class="custom-checkbox"></div>');
-
-      checkboxes.bind('updateState', function(){
-        var label = $('label[for='+ $(this).attr('id') +']');
-        if ( $(this).is(':checked') ){
-          label.addClass('checked');
-        } else {
-          label.removeClass('checked checkedHover checkedFocus');
-        }
-      });
-
-      checkboxes.trigger('updateState');
-
-      checkboxes.click(function(){
-        $(this).trigger('updateState');
-      });
-
-      checkboxes.focus(function(){
-        var label = $('label[for='+ $(this).attr('id') +']');
-        label.addClass('focus');
-        if( $(this).is(':checked') ){
-          $(this).addClass('checkedFocus');
-        }
-      });
-
-      checkboxes.blur(function() {
-        var label = $('label[for='+ $(this).attr('id') +']');
-        label.removeClass('focus checkedFocus');
-      });
+  checkboxes.each(function(){
+    var label = $('label[for='+ $(this).attr('id') +']');
+    label.mouseover(function(){
+      $(this).addClass('hover');
+      if( $(this).is('.checked') ){
+        $(this).addClass('checkedHover');
+      }
     });
- 
-		
+    label.mouseout(function(){
+      $(this).removeClass('hover checkedHover');
+    });
 
+    $(this).add('label').wrap('<div class="custom-checkbox"></div>');
+
+    checkboxes.bind('updateState', function(){
+      var label = $('label[for='+ $(this).attr('id') +']');
+      if ( $(this).is(':checked') ){
+        label.addClass('checked');
+      } else {
+        label.removeClass('checked checkedHover checkedFocus');
+      }
+    });
+
+    checkboxes.trigger('updateState');
+
+    checkboxes.click(function(){
+      $(this).trigger('updateState');
+    });
+
+    checkboxes.focus(function(){
+      var label = $('label[for='+ $(this).attr('id') +']');
+      label.addClass('focus');
+      if( $(this).is(':checked') ){
+        $(this).addClass('checkedFocus');
+      }
+    });
+
+    checkboxes.blur(function() {
+      var label = $('label[for='+ $(this).attr('id') +']');
+      label.removeClass('focus checkedFocus');
+    });
+  });
 	
   $(".combobox").selectmenu({
     width: 457
   });
-	
-//  $('ul.navbar')
-//    .find('a[href="'+ document.location.pathname +'"]')
-//    .closest('li')
-//    .addClass('current')
-//  ;
  
   $('a[rel=external]').click(function() {
     window.open(this.href);
