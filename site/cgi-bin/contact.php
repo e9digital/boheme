d9f3f907e4c9dd4e9083727e3c030f175548de6c
Travis <numbers1311407@gmail.com>
2011-04-04 12:20:05 -0400

    contact form real email

--- site/cgi-bin/contact.php
+++ site/cgi-bin/contact.php
@@ -1,22 +1,21 @@
 <?
   if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
-    $to       = 'numbers1311407@gmail.com';
+    $to       = 'Boheme Contact Form <info@bohemefilms.com>';
     $subject  = "boheme contact form: from " . substr($_POST['email'], 0, 30);
     
     $body     = "";
     $body    .= "First Name: "  . $_POST['first-name']  . "\r\n";
     $body    .= "Last Name: "  . $_POST['last-name']  . "\r\n";
     $body    .= "Email: " . $_POST['email'] . "\r\n";
-    $body    .= "Should Contact?: " . "\r\n" . $_POST['contact-agreement'] . "\r\n";
-    $body    .= "\r\n";
-    $body    .= "Primary Reason: " . "\r\n" . $_POST['primary-reason'] . "\r\n";
-    $body    .= "Other Reason: " . "\r\n" . $_POST['other-reason'] . "\r\n";
-    $body    .= "Message: " . "\r\n" . $_POST['message'] . "\r\n";
+    $body    .= "Should Contact: " . $_POST['contact-agreement'] . "\r\n";
+    $body    .= "Primary Reason: " . $_POST['primary-reason'] . "\r\n";
+    $body    .= "Other Reason: " . $_POST['other-reason'] . "\r\n";
+    $body    .= "Message: " . $_POST['message'] . "\r\n";
 
     $headers  = "";
     $headers .= "Mime-Version: 1.0" . "\r\n";
     $headers .= "Content-Type: text/plain; CHARSET=iso-8859-1" . "\r\n";
-    $headers .= "From: Boheme Contact Form <" . $to . ">\r\n";
+    $headers .= "From: " . $to . "\r\n";
     $headers .= "Reply-To: " . $_POST['email'] . "\r\n";
 
     if (mail($to, $subject, $body, $headers)) {
@@ -27,4 +26,4 @@
   } else {
     header("Status: 404 Not Found");
   }
-?
+?>
